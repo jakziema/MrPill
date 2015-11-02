a004b644614f065e41f04a6165fec40f999f9b34
Jakub <ziemannjakub@gmail.com>
2015-11-02 11:59:15 +0100

    delete

--- Pods/Target Support Files/Pods-MrPill_Parsecoco/Pods-MrPill_Parsecoco-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_MrPill_ParsecocoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MrPill_ParsecocoVersionString[];
-
--- README.md
+++
@@ -1,2 +0,0 @@
-Mr Pill
-AppCamp2015
