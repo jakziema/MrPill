31ffb521f8321169badee574fb1366844837b697
Jakub <ziemannjakub@gmail.com>
2015-11-07 14:39:05 +0100

    WatchOS2 again

---
+++ MrPill_Parsecoco/MedicineTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MedicineTableViewCell.swift
+//  MrPill_Parsecoco
+//
+//  Created by appcamp on 10/8/15.
+//  Copyright © 2015 appcamp. All rights reserved.
+//
+
+import UIKit
+
+
+class MedicineTableViewCell: UITableViewCell {
+    @IBOutlet weak var medicineLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+}
---
+++ MrPill_Parsecoco/UIViewControllerExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewControllerExtension.swift
+//  MrPill_Parsecoco
+//
+//  Created by appcamp on 10/8/15.
+//  Copyright © 2015 appcamp. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showErrorView(error: NSError) {
+        if let errorMessage = error.userInfo["error"] as? String {
+            let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+}
